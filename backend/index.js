d562afd2a8fd2f553905e8b528e5a2f759013397
jeremias colina mansilla <jeremiascolina20@gmail.com>
2025-11-03 22:47:08 -0300

    subiendo carpeta de routes con materias.js y validaciones.js, tambien una carpeta para guardar todos los http del proyecto, por ahora subiendo el http de materias

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import express from "express"
 import { conectarDB } from "./db.js"
 import cors from "cors"
 
-
+import materiaRoutes from "./routes/materias.js"
 conectarDB();
 
 const app = express();
@@ -14,13 +14,21 @@ app.use(express.json())
 
 app.use(cors())
 
+app.use("/materias", materiaRoutes)
 
 app.get("/", (req, res)=>{
     res.send("notas para los alumnos")
 })
 
 
-
+// Middleware para manejar errores del servidor (500)
+app.use((err, req, res, next) => {
+  console.error("🔥 Error interno del servidor:", err);
+  res.status(500).json({
+    success: false,
+    message: "Error interno del servidor. Intente nuevamente más tarde.",
+  });
+});
 
 app.listen(port, ()=>{
     console.log(`la aplicacion esta funcionando en el puerto ${port}`)
---
+++ backend/routes/materias.js
@@ -0,0 +1,138 @@
+import express from "express"
+import { db } from "../db.js"
+import { validarId, verificarValidaciones } from "./validaciones.js"
+import { body, param } from "express-validator"
+const router = express.Router()
+
+
+
+// obtener todas la materias
+router.get("/", async(req, res)=>{
+    const [rows] = await db.query("SELECT * FROM materia");
+    res.json(rows)
+})
+
+//materia por id
+router.get(
+    "/:id",
+    validarId,
+    verificarValidaciones, 
+    async (req, res)=>{
+        const id = Number(req.params.id);
+        const [rows] = await db.query("SELECT * FROM materia WHERE id = ?", [id]);
+
+        if (rows.length === 0 ) {
+            return res.status(404)
+            .json({success: false, message:"Materia no encontrada"})
+        }
+        res.json({success: true, data: rows[0]})
+    }
+)
+
+
+
+//post para crear las materias
+router.post(
+    "/",
+    body("nombre").isString().isLength({min: 3, max: 100}),
+    body("codigo").isInt({min: 1}),
+    body("año").isInt({min: 1, max: 6}),
+    verificarValidaciones, async (req, res)=>{
+        const {nombre, codigo, año }= req.body;
+
+        const [existe] = await db.query(
+            "SELECT * FROM materia WHERE codigo = ? or nombre = ?",
+            [codigo, nombre]
+        );
+        if (existe.length > 0) {
+            return res.status(400).json({
+                success: false,
+                message: "Ya existe una materia con ese nombre o codigo",
+            })
+        }
+ 
+
+        const [result] = await db.query(
+            "INSERT INTO materia (nombre, codigo, año) VALUES (?,?,?)",
+            [nombre, codigo, año]
+        );
+        res.status(201).json({
+            success: true,
+            data: {id: result.insertId, nombre, codigo, año}
+        })
+    }
+)
+
+
+// Actualizar una materia por id
+router.put(
+    "/:id",
+    validarId,
+    body("nombre").optional().isString().isLength({min: 3, max: 100}),
+    body("codigo").optional().isInt({min: 1 }),
+    body("año").optional().isInt({min: 1, max: 6}),
+    verificarValidaciones,
+    async (req, res)=>{
+        const id = Number(req.params.id);
+        const { nombre, codigo, año } = req.body;
+
+
+        const [rows] = await db.query(
+            "SELECT * FROM materia WHERE id = ?", [id]);
+
+        if (!nombre && !codigo && !año){
+            return res.status(400).json({
+                success: false,
+                message: "Debe enviar almenos un campos para actualizar "
+            })
+        }
+
+        if (rows.length === 0 ) {
+            return res.status(404)
+            .json({success:false, message: "Materia no encontrada"})
+        }
+        
+        
+        await db.query(
+            "UPDATE materia SET nombre = ?, codigo = ?, año = ? WHERE id = ?",
+            [
+                nombre ?? rows[0].nombre,
+                codigo ?? rows[0].codigo,
+                año ?? rows[0].año,
+                id,
+            ]
+        );
+        res.json({
+            success: true,
+            message: "Materia actualizada correctamente",
+            data: {id, nombre: nombre ?? rows[0].nombre,
+                    codigo: codigo ?? rows[0].codigo,
+                    año: año ?? rows[0].año }
+        });
+    }
+);
+
+
+//Eliminar una materia por id 
+router.delete(
+    "/:id",
+    validarId,
+    verificarValidaciones,
+    async (req, res )=>{
+        const id = Number(req.params.id);
+
+        const [rows]= await db.query("SELECT * FROM materia WHERE id = ?", [id]);
+        if (rows.length === 0){
+            return res.status(404)
+            .json({success: false, message: "Materia no encontrada"});
+        }
+        await db.query("DELETE FROM materia WHERE id = ?", [id])
+        res.json({
+            success: true,
+            message: "Materia eliminada correctamente"
+        })
+    }
+)
+
+
+export default router
